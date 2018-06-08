f4c92557e2387188fb1dfffdc49567aa9802a3a2
Daniel Mancera <daniel.mancera@crg.eu>
2018-06-08 14:20:31 +0200

    add a sample type filter

--- src/app/administration/context-source/peptides-list/peptides-list.component.ts
+++ src/app/administration/context-source/peptides-list/peptides-list.component.ts
@@ -4,6 +4,10 @@ import { SampleCompositionService } from '../../../services/sample-composition.s
 import { Peptide } from '../../../models/peptide';
 import { SampleComposition } from '../../../models/sampleComposition';
 import { Subscription } from 'rxjs/Subscription';
+import { SampleType } from '../../../models/sampleType';
+import { SampleTypeService } from '../../../services/sample-type.service';
+import { delay } from 'q';
+declare var M: any;
 
 /**
  * Peptide list component
@@ -17,21 +21,44 @@ import { Subscription } from 'rxjs/Subscription';
 export class PeptidesListComponent implements OnInit, OnDestroy {
 
   constructor(private peptideService: PeptideService,
-    private sampleCompositionService: SampleCompositionService) { }
+    private sampleCompositionService: SampleCompositionService,
+    private sampleTypeServce: SampleTypeService) { }
 
-  peptides = [];
+  peptides: Peptide[] = [];
+
+  sampleTypes: SampleType[] = [];
 
   peptideFromDb$: Subscription;
 
   ngOnInit() {
-    this.loadPeptides();
+    this.loadSampleTypes();
+    this.loadPeptides('All');
     // Observing the incomming peptides from the form
     this. peptideFromDb$ = this.peptideService.peptideFromDb$
       .subscribe(
         (peptide) => {
           this.peptides = [];
-          this.loadPeptides();
+          this.loadPeptides('All');
         });
+  }
+
+  private loadSampleTypes(): void {
+    this.sampleTypeServce.getSamplesTypes()
+      .subscribe(
+        (sampleTypes) => {
+          this.sampleTypes = sampleTypes;
+        }, err => console.log(err),
+        () => {
+          delay(100).then(() => {
+            this.enableSelect();
+          });
+        }
+      );
+  }
+
+  private enableSelect(): void {
+    const elem = document.getElementById('sampleTypeSelector');
+    const instance = M.FormSelect.init(elem, {});
   }
 
   ngOnDestroy() {
@@ -51,22 +78,36 @@ export class PeptidesListComponent implements OnInit, OnDestroy {
   }
 
   /**
-   * Get all peptides from the database and
+   * Get All peptides from the database and
    * where they are comming from
+   * @param sampleTypeName the name of the sampletype
    */
-  private loadPeptides(): void {
+  private loadPeptides(sampleTypeName: string): void {
     this.peptides = [];
-    this.peptideService.getAllPeptides()
+    if (sampleTypeName === 'All') {
+      this.peptideService.getAllPeptides()
       .subscribe((peptides) => {
-        console.log(peptides);
         peptides.forEach(peptide => {
           // get the belongs
           peptide['belongs'] = null;
           this.getSampleType(peptide);
         });
       },
-        error => console.log(error),
-      () => console.log('end'));
+        error => console.log(error));
+    } else {
+      const sampleType = this.sampleTypes.find((st) => st.name === sampleTypeName);
+      console.log(sampleType);
+      this.sampleCompositionService.getAllPeptidesBySampleType(sampleType)
+        .subscribe(
+          (peptides) => {
+            peptides.forEach(peptide => {
+              // get the belongs
+              peptide['belongs'] = null;
+              this.getSampleType(peptide);
+            });
+          }
+        );
+    }
   }
   /**
    * Set the peptide sample type
@@ -103,6 +144,10 @@ export class PeptidesListComponent implements OnInit, OnDestroy {
    */
   sendToDetail(peptide: Peptide): void {
     this.peptideService.sendPeptide(peptide);
+  }
+
+  doFilter(sampleType: string): void {
+    this.loadPeptides(sampleType);
   }
 
 }
