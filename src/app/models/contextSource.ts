ce1d780c1254d339a097d03a19d3d9a62c1461d8
Daniel Mancera <daniel.mancera@crg.eu>
2018-06-12 09:02:20 +0200

    change context source model

--- src/app/models/contextSource.ts
+++ src/app/models/contextSource.ts
@@ -1,9 +1,11 @@
 export class ContextSource {
     id: number;
     name: string;
+    abbreviated: string;
 
-    constructor(id: number, name: string) {
+    constructor(id: number, name: string, abbreviated: string) {
         this.id = id;
         this.name = name;
+        this.abbreviated = abbreviated;
     }
 }
