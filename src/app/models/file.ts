5645a6bcd3ab3bf559f69ebd1fcf673995c41634
Daniel Mancera <daniel.mancera@crg.eu>
2018-06-15 15:08:10 +0200

    add file model

---
+++ src/app/models/file.ts
@@ -0,0 +1,20 @@
+import { SampleType } from './sampleType';
+import { System } from './system';
+
+export class File {
+    id: number;
+    sampleType: SampleType;
+    system: System;
+    creationDate: Date;
+    filename: string;
+    checksum: string;
+
+    constructor(id: number, sampleType: SampleType, system: System, creationDate: Date, filename: string, checksum: string) {
+        this.id = id;
+        this.sampleType = sampleType;
+        this.system = system;
+        this.creationDate = creationDate;
+        this.filename = filename;
+        this.checksum = checksum;
+    }
+}
