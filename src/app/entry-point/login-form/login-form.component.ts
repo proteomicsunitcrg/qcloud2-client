f359e1719207ecbc7a0f7ee55d22a61e74b8cf0d
Daniel Mancera <daniel.mancera@crg.eu>
2018-09-19 11:52:24 +0200

    add proper navigation to default view

--- src/app/entry-point/login-form/login-form.component.ts
+++ src/app/entry-point/login-form/login-form.component.ts
@@ -3,7 +3,7 @@ import { FormGroup } from '@angular/forms/src/model';
 import { AuthService } from '../../auth.service';
 import * as decode from 'jwt-decode';
 import { Router } from '@angular/router';
-import {trigger, animate, style, group, animateChild, query, stagger, transition} from '@angular/animations';
+import { UserDefaultViewService } from '../../services/user-default-view.service';
 /**
  * Login component
  * @author Daniel Mancera <daniel.mancera@crg.eu>
@@ -20,16 +20,24 @@ export class LoginFormComponent implements OnInit {
   username: string;
   password: string;
   serverOutput = '';
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService,
+    private router: Router,
+    private userDefaultViewService: UserDefaultViewService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Check if token already exists
+    if (this.authService.isLoggedIn()) {
+      this.doNavigation();
+    }
+  }
 
   onSubmit(): void {
     this.authService.login(this.username, this.password)
       .subscribe(
       (res) => {
         this.authService.setSession(res);
-        this.router.navigate(['application']);
+        // this.router.navigate(['application']);
+        this.doNavigation();
       },
       (error) => {
         console.log(error);
@@ -37,6 +45,24 @@ export class LoginFormComponent implements OnInit {
 
       });
   }
+
+  private doNavigation(): void {
+    this.userDefaultViewService.findDefaultView()
+      .subscribe(
+        (userDefaultView) => {
+          if (userDefaultView === null) {
+            this.router.navigate(['application']);
+          } else {
+            if (userDefaultView.viewType === 'INSTRUMENT') {
+              this.router.navigate(['application/view/instrument/' + userDefaultView.labSystem.apiKey]);
+            } else {
+              this.router.navigate(['application/view/user/' + userDefaultView.view.apiKey]);
+            }
+          }
+        }
+      );
+  }
+
   doLogout(): void {
     console.log('logout');
     this.authService.logout();
