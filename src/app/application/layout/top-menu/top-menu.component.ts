730653f8be9bc56f745913b1b7e9406bba73f8b6
Daniel Mancera <daniel.mancera@crg.eu>
2018-10-15 14:35:42 +0200

    removes websocket message

--- src/app/application/layout/top-menu/top-menu.component.ts
+++ src/app/application/layout/top-menu/top-menu.component.ts
@@ -43,7 +43,7 @@ export class TopMenuComponent implements OnInit, OnDestroy {
     this.loadNodeSystems();
     this.loadUserViews();
     this.subscribeToNewUserView();
-    this.initializeWebSocket();
+    // this.initializeWebSocket();
   }
 
   ngOnDestroy() {
