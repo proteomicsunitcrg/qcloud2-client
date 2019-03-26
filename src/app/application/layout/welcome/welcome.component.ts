48c8ddc75b65c58a8e898104e3371b0623498ac3
Marc Serret <marc.serret@crg.eu>
2019-03-26 11:33:39 +0100

    Added videos in home page

--- src/app/application/layout/welcome/welcome.component.ts
+++ src/app/application/layout/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-welcome',
@@ -7,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public sanitizer: DomSanitizer) { }
+  videoLinks = [
+    { 
+      title: "Getting started as a user",
+      link: "https://drive.google.com/file/d/1nHgbWmRltToAEvBF9J4_kd7k2q0mx_Pi/preview"
+    },
+    { 
+      title: "Getting started as a manager",
+      link: "https://drive.google.com/file/d/10VhYb-q_Usrp-gCTVSpQf3u2fBZK7oEp/preview"
+    },
+    { 
+      title: "Thresholds and guidesets",
+      link: "https://drive.google.com/file/d/1WCmGel417NYIZiyfIFLsln98dSDNKwP4/preview"
+    },
+  ];
   ngOnInit() {
   }
 
