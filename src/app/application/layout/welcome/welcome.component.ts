746179b7e3420002deacdb85c809f7321b8e6986
Marc Serret <marc.serret@crg.eu>
2019-04-03 16:33:37 +0200

    Tutorial videos now are thumbnails with Drive links

--- src/app/application/layout/welcome/welcome.component.ts
+++ src/app/application/layout/welcome/welcome.component.ts
@@ -11,18 +11,22 @@ import { Message } from '../../../models/message';
 export class WelcomeComponent implements OnInit {
 
   constructor(public sanitizer: DomSanitizer, private msgService: MessageService) { }
+  imgUrl = "assets/images/thumbnails/thumb";
   videoLinks = [
     { 
       title: "Getting started as a user",
-      link: "https://drive.google.com/file/d/1nHgbWmRltToAEvBF9J4_kd7k2q0mx_Pi/preview"
+      link: "https://drive.google.com/file/d/1nHgbWmRltToAEvBF9J4_kd7k2q0mx_Pi/view?usp=sharing",
+      img: this.imgUrl + "User.png"
     },
     { 
-      title: "Getting started as a manager",
-      link: "https://drive.google.com/file/d/10VhYb-q_Usrp-gCTVSpQf3u2fBZK7oEp/preview"
+      title: "Getting started as a lab manager",
+      link: "https://drive.google.com/file/d/10VhYb-q_Usrp-gCTVSpQf3u2fBZK7oEp/view?usp=sharing",
+      img: this.imgUrl + "LabManager.png"
     },
     { 
       title: "Thresholds and guidesets",
-      link: "https://drive.google.com/file/d/1WCmGel417NYIZiyfIFLsln98dSDNKwP4/preview"
+      link: "https://drive.google.com/file/d/1WCmGel417NYIZiyfIFLsln98dSDNKwP4/view?usp=sharing",
+      img: this.imgUrl + "Thres.png"
     },
   ];
   message = new Message("title", "text","error", false);
