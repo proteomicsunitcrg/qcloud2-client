3ceb63adfc1c7a2e496761dc14cf2a89b8abed23
Marc Serret <marc.serret@crg.eu>
2019-05-14 14:42:12 +0200

    Update doc

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -11,16 +11,26 @@ export class EmailService {
 
   constructor(private httpClient: HttpClient) { }
 
+  // ENV URL
   private apiPrefix = environment.apiPrefix;
-
+  
+  // Email API URL
   emailUrl = this.apiPrefix + 'api/email';
 
-  public sendEmail(email: Email): Observable<any> {
+  /**
+   * @summary Sends a POST petition to the server to send an email
+   * @author Marc Serret
+   * @since 1.0.0
+   * @param email Email object to send to the server
+   * @returns An observable boolean with the request status
+   * @access public
+   */
+  public sendEmail(email: Email): Observable<boolean> {
     console.log(email);
     const json = JSON.stringify(email);
     const params = json;
     const headers = new HttpHeaders().set('Content-type', 'application/json');
-    return this.httpClient.post<Email>(this.emailUrl + '/send', params, {headers: headers});
+    return this.httpClient.post<boolean>(this.emailUrl + '/send', params, {headers: headers});
   }
 
 
