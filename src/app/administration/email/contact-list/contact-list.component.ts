63e0a2c0ebaee79f5bbc693e729594aaa02be6d4
Marc Serret <marc.serret@crg.eu>
2019-05-22 12:33:25 +0200

    SelectAll/unselectAll feature mail list

--- src/app/administration/email/contact-list/contact-list.component.ts
+++ src/app/administration/email/contact-list/contact-list.component.ts
@@ -16,21 +16,69 @@ export class ContactListComponent implements OnInit {
   ngOnInit() {
     this.getAllNodes();
   }
-
+  /**
+  * @summary Manage the selection
+  * @description Add the element to selected list or removes it
+  * @param i position of the element in the list
+  * @typeparam i number
+  * @author Marc Serret
+  * @since 1.0.0
+  * @access public
+  */
   onSelect(i: number): void {
     const contact = document.getElementsByClassName('contactList')[i];
     if (!this.selected.includes(contact.textContent) && !this.isTitle(contact.textContent)) {
       contact.classList.add('selected');
       this.selected.push(contact.textContent);
-      // console.log("pushed")
     } else {
       this.selected = this.selected.filter(item => item !== contact.textContent);
       contact.classList.remove('selected');
-      // console.log("deleted")
     }
     this.emitList();
   }
-
+  /**
+  * @summary Add all the users to the selected array
+  * @description Add all the users to the selected array and sets all the users to selected css class, also emits the list
+  * @author Marc Serret
+  * @since 1.0.0
+  * @access public
+  */
+  selectAll() {
+    const contactList: any = document.getElementsByClassName('contactList');
+    for (let contact of contactList) {
+      if (!this.isTitle(contact.textContent) && !this.selected.includes(contact.textContent)) {
+        contact.classList.add('selected');
+        this.selected.push(contact.textContent);
+      }
+    }
+    this.emitList();    
+  }
+  
+  /**
+  * @summary Remove all the users from the selected array
+  * @description Remove all the users from the selected array and removes all the users the selected css class, also emits the list
+  * @author Marc Serret
+  * @since 1.0.0
+  * @access public
+  */
+  unselectAll() {
+    const contactList: any = document.getElementsByClassName('contactList');
+    this.selected.splice(0, this.selected.length);
+    for(let contact of contactList) {
+      contact.classList.remove('selected');
+    }
+    this.emitList();
+  }
+  /**
+  * @summary Check if the string is a title
+  * @description Check if the string is a title based on the arroba char
+  * @param name the name to check
+  * @typeparam name string
+  * @author Marc Serret
+  * @returns true if int't a title or false otherwise
+  * @since 1.0.0
+  * @access public
+  */
   isTitle(name: string): boolean {
     if (name.includes('@')) {
       return false;
@@ -38,11 +86,23 @@ export class ContactListComponent implements OnInit {
       return true;
     }
   }
-
+  /**
+  * @summary Emits the selected users list
+  * @description Emits the selected users list to the father component
+  * @author Marc Serret
+  * @since 1.0.0
+  * @access public
+  */
   emitList() {
     this.contactEmitter.emit(this.selected);
   }
-
+  /**
+  * @summary Get all nodes and users
+  * @description Get all nodes and users
+  * @author Marc Serret
+  * @since 1.0.0
+  * @access private
+  */
   private getAllNodes() {
     this.userService.getAllNodes().subscribe(
       (nodes) => {
