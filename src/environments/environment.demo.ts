0ff335d8fed708d1ab504667565bbaee2fe6aacc
Marc Serret <vesperon51@gmail.com>
2020-12-17 11:14:18 +0100

    env

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiPrefix: 'http://localhost:8082/',
+  name: 'outside' // IMPORTANT if you change this remember that in the file /administration/sidebar.ts change the if check
+};
