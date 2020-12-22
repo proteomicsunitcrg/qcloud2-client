cdccd844290293628b8eb0713d4d2060136be6cf
Marc Serret <vesperon51@gmail.com>
2020-12-22 13:31:55 +0100

    Demo backend url

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiPrefix: 'http://localhost:8082/',
+  apiPrefix: 'http://demo.qcloud2.crg.eu/',
   name: 'outside' // IMPORTANT if you change this remember that in the file /administration/sidebar.ts change the if check
 };
