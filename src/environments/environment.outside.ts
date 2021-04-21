2af96eb7f67afb8e916c21c0cdcdc6510f8d56e0
Marc Serret <vesperon51@gmail.com>
2021-04-21 10:30:10 +0200

    Staged

--- src/environments/environment.outside.ts
+++ src/environments/environment.outside.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiPrefix: 'https://odcf-qcloud2.dkfz.de/',
+  apiPrefix: 'http://tamarin/',
   name: 'outside' // IMPORTANT if you change this remember that in the file /administration/sidebar.ts change the if check
 };
