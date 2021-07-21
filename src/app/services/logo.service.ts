c19d76466a3e30a9bba81d27e6c05d9c6a90214a
Marc Serret <marc.serret@crg.eu>
2021-07-21 09:32:53 +0200

    Links

--- src/app/services/logo.service.ts
+++ src/app/services/logo.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Logo } from '../models/logo';
+import { Logo } from '../models/Logo';
 @Injectable({
   providedIn: 'root'
 })
