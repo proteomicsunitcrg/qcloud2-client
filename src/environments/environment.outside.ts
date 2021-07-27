ce6664346106f583e815f2d4f34a368b5b20277c
Marc Serret <vesperon51@gmail.com>
2021-07-27 09:33:51 +0200

    URL

--- src/environments/environment.outside.ts
+++ src/environments/environment.outside.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiPrefix: 'http://tamarin:8089/',
+  apiPrefix: 'http://protmsct1.helmholtz-muenchen.de:8089/',
   name: 'outside' // IMPORTANT if you change this remember that in the file /administration/sidebar.ts change the if check
 };
